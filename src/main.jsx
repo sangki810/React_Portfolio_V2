0a13bcd5e3ffb772436787aa65e55d26a5178bf8
Sangki Bryan Jun <sangki810@gmail.com>
2026-06-12 18:10:03 -0400

    deleted unused import

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter } from 'react-router-dom';
-import ReactDOM from 'react-dom/client';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
