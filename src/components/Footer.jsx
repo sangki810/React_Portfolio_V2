f1df5d2dcb32df65da41cfdf76ccfe0364616807
Sangki Bryan Jun <sangki810@gmail.com>
2026-06-16 15:26:19 -0400

    fixed home page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
 
 const Footer = () => {
-    return (
-        <footer className='mt-8 border-t z-10 border-t-[#33353F] border-l-transparent
-            border-r-transparent text-white'>
-            <div className='container p-4 md:p-12 justify-between flex flex-col md:flex-row 
-                items-center'>
-                <div className='text-white text-2xl md:text-3xl font-black cursor-pointer gap-4 md:gap-0'>
-                    PORTFOLIO<span className='text-primary'>.</span>
-                </div>
-                <p className='text-slate-600 text-sm md:text-base'>
-                    &copy; All Rights Reserved
-                </p>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className='mt-8'>
+      <div className='w-full flex justify-center items-center py-6'>
+        <p className='text-slate-600 text-sm md:text-base text-center'>
+          Sangki Bryan Jun &copy; All Rights Reserved
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,7 +5,7 @@ import resume from '../assets/Sangki_Jun_Resume.pdf';
 
 const HeroSection = () => {
   return (
-    <section className='relative w-full' data-aos='zoom-in-up'>
+    <section className='relative w-full pb-10' data-aos='zoom-in-up'>
         <div className='absolute top-0 inset-x-0 h-64 flex items-start'>
             <div className='h-24 w-2/3 bg-linear-to-br from-[#0c7fac] blur-2xl invisible opacity-40'></div>
             <div className='h-20 w-3/4 bg-linear-to-r from-[#289eff] opacity-40 blur-2xl'></div>
