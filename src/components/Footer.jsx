2ddddceaf906577091ac81f8f52b074ea8fd6da7
Sangki Bryan Jun <sangki810@gmail.com>
2026-06-16 18:26:25 -0400

    updated footer.jsx with current year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className='mt-8'>
       <div className='w-full flex justify-center items-center py-6'>
         <p className='text-slate-600 text-sm md:text-base text-center'>
-          Sangki Bryan Jun &copy; All Rights Reserved
+          Sangki Bryan Jun &copy; All Rights Reserved {new Date().getFullYear()}
         </p>
       </div>
     </footer>
